ca6ae153c294eb286bd1b83b488c2c65e610258b
Tushar-Tiwari1415 <2k23.csdsc2312468@gmail.com>
2024-12-14 02:04:23 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    var typed = new Typed('#element', {
+      strings: ['Web Developer','Graphic Designer','Video Editor','Data Analyst','Nodejs Developer'],
+      typeSpeed: 50,
+    });
+  
